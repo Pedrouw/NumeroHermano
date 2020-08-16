a7f1a49e3d2fc4c8c230b0dabff83a5dae141715
Javiera Letelier <javieralete@gmail.com>
2020-08-16 15:55:43 -0400

    GUI 2.0
    para que se vea mejor

--- Primoinador/GUI.py
+++ Primoinador/GUI.py
@@ -3,34 +3,33 @@ from tkinter import scrolledtext
 
 ventana = Tk()
 ventana.title("Primoinador")
-ventana.geometry('600x350')
-
-prs = Label(ventana, text=" ")
-prs.grid(column=0, row=0)
+ventana.geometry('540x480')
+ventana.configure(width=540, height=480)
+ventana.resizable(False, False)
 
 texto_sup = Label(ventana, text="El Primoinador!", font=("Consolas", 15))
-texto_sup.grid(column=4, row=1)
+texto_sup.pack()
 
 texto_inf = Label(ventana, text="El ''-inador'' para encontrar números primos", font=("Consolas", 10))
-texto_inf.grid(column=4, row=2)
+texto_inf.pack()
 
 dsd = Label(ventana, text="Desde el: ", font=("Consolas", 9))
-dsd.grid(column=2, row=5)
+dsd.place(x=98, y=120)
 
 num1_inpt = Entry(ventana, width=10)
-num1_inpt.grid(column=3, row=5)
+num1_inpt.place(x=188, y=120)
 
 hst = Label(ventana, text=" hasta el: ", font=("Consolas", 9))
-hst.grid(column=4, row=5)
+hst.place(x=278, y=120)
 
 num2_inpt = Entry(ventana, width=10)
-num2_inpt.grid(column=5, row=5)
+num2_inpt.place(x=368, y=120)
 
 act_btn = Button(ventana, text="Encontrar primos")
-act_btn.grid(column=4, row=7)
+act_btn.place(x=234, y=180)
 
 rslt = scrolledtext.ScrolledText(ventana, width=40, height=10)
-rslt.grid(column= 4, row=9)
+rslt.place(x=117, y=240)
 rslt.config(state=DISABLED)
 
 ventana.mainloop()
