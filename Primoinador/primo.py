515b0f27010396197467010e81c9f8080eb3e9c7
Javiera Letelier <javieralete@gmail.com>
2020-08-21 19:45:34 -0400

    todo lo que pude hacer
    continuaré mañana

--- Primoinador/primo.py
+++ Primoinador/primo.py
@@ -11,8 +11,8 @@ def nprimos():
                 primos = False
             else:
                 continue
-        if primos == True:
-            resultado = print(' ', i, end='')
+        if primos:
+            print(' ', i, end='')
             cont += 1
 
     if num1 >= num2:
